31820029f66f2659310b8b0e2ff61678bebb6de3
Pedro Santana <pedromanu27@gmail.com>
2022-01-30 22:47:04 -0300

    comunicação

---
+++ componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do Usuário: <strong>{{ inverterNome() }}</strong>
+    </p>
+    <button @click="reinicializarNome">Reinicializar Nome</button>
+    <button @click="reiniciarFn">ReiniciarNome (Callback)</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nome: {
+      type: String,
+      // required: true,
+      // default: function () {
+      //     return Array(10).fill(0).join(',')
+      // }
+      default: "Anonimo"
+    },
+    reiniciarFn: Function
+  },
+  methods: {
+    inverterNome() {
+      return this.nome.split("").reverse().join("");
+    },
+    reinicializarNome() {
+    //   this.nome = "Pedro";
+      this.$emit("nomeMudou", 'Pedro');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
