010249cc3d8bcc5454ae8d9a65c346234fe08c80
Pedro Santana <pedromanu27@gmail.com>
2022-03-05 01:19:43 -0300

    formulários

--- formularios/formulario-exercicios/src/App.vue
+++ formularios/formulario-exercicios/src/App.vue
@@ -5,35 +5,49 @@
 			<form class="painel">
 				<div class="cabecalho">Formulário</div>
 				<Rotulo nome="E-mail">
-					<input type="text" v-model.lazy.trim="usuario.email">
+					<input type="text" v-model.lazy.trim="usuario.email" />
 				</Rotulo>
 				<Rotulo nome="Senha">
-					<input type="password" v-model="usuario.senha">
+					<input type="password" v-model="usuario.senha" />
 				</Rotulo>
 				<Rotulo nome="Idade">
-					<input type="number" v-model.number="usuario.idade">
+					<input type="number" v-model.number="usuario.idade" />
 				</Rotulo>
 				<Rotulo nome="Mensagem">
-					<textarea name="" cols="30" rows="5"></textarea>
+					<textarea name cols="30" rows="5" v-model="mensagem"></textarea>
 				</Rotulo>
 				<Rotulo nome="Características do Problema">
-					<span class="mr-4"><input type="checkbox" value="reproduzivel"> Reproduzível</span>
-					<span><input type="checkbox" value="intermitente"> Intermitente</span>
+					<span class="mr-4">
+						<input type="checkbox" value="reproduzivel" v-model="caracteristicas" /> Reproduzível
+					</span>
+					<span>
+						<input type="checkbox" value="intermitente" v-model="caracteristicas" /> Intermitente
+					</span>
 				</Rotulo>
 				<Rotulo nome="Qual produto?">
-					<span class="mr-4"><input type="radio"> Web</span>
-					<span class="mr-4"><input type="radio"> Mobile</span>
-					<span><input type="radio"> Outro</span>
+					<span class="mr-4">
+						<input type="radio" value="web" v-model="produto" /> Web
+					</span>
+					<span class="mr-4">
+						<input type="radio" value="mobile" v-model="produto" /> Mobile
+					</span>
+					<span>
+						<input type="radio" value="outro" v-model="produto" /> Outro
+					</span>
 				</Rotulo>
 				<Rotulo nome="Prioridade">
-					<select name="" id="">
-						<option></option>
+					<select v-model="prioridade">
+						<option v-for="prioridade in prioridades"
+						:value="prioridade.codigo" 
+						:key="prioridade.codigo"
+						:selected="prioridade.codigo === 2">{{ prioridade.nome }}</option>
 					</select>
 				</Rotulo>
 				<Rotulo nome="Primeira Reclamação?">
-					<Escolha />
+					<!-- <input type="text" :value="temp" @input="temp = $event.target.value"> -->
+					<Escolha v-model="escolha" />
 				</Rotulo>
-				<hr>
+				<hr />
 				<button>Enviar</button>
 			</form>
 			<div class="painel">
@@ -48,20 +62,27 @@
 					<span>{{ usuario.idade }} {{ tipoIdade }}</span>
 				</Rotulo>
 				<Rotulo nome="Mensagem">
-					<span>???</span>
+					<span style="white-space: pre;">{{ mensagem }}</span>
 				</Rotulo>
-				<Rotulo nome="Marque as Opções">
-					<span>???</span>
+				<Rotulo nome="Características do Problema">
+					<span>
+						<ul>
+							<li v-for="(c, i) in caracteristicas" :key="i">{{ i }} - {{ c }}</li>
+						</ul>
+					</span>
 				</Rotulo>
 				<Rotulo nome="Qual produto?">
-					<span>???</span>
+					<span>{{ produto }}</span>
 				</Rotulo>
 				<Rotulo nome="Prioridade">
-					<span>???</span>
+					<span>{{ prioridade }}</span>
 				</Rotulo>
 				<Rotulo nome="Primeira Reclamação?">
-					<span>???</span>
+					<span>{{ escolha }}</span>
 				</Rotulo>
+				<!-- <Rotulo nome="Temp">
+					<span>{{ temp }}</span>
+				</Rotulo> -->
 			</div>
 		</div>
 	</div>
@@ -81,24 +102,33 @@ export default {
 	},
 	data() {
 		return {
+			mensagem: '',
+			caracteristicas: [],
+			produto: 'web',
+			prioridades: [
+				{ codigo: 1, nome: 'Baixa' },
+				{ codigo: 2, nome: 'Moderada' },
+				{ codigo: 3, nome: 'Alta' },
+			],
+			prioridade: 1,
 			usuario: {
 				email: '',
 				senha: '',
-				idade: 25
-			}
+				idade: 25,
+			},
+			escolha: true
 		}
 	}
 }
 </script>
 
 <style>
-
 body {
-	background-color: #ECECEC;
+	background-color: #ececec;
 }
 
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
@@ -114,16 +144,16 @@ body {
 
 .painel {
 	flex: 1;
-	background: #FFF;
+	background: #fff;
 	margin: 0px 10px;
 	padding: 20px;
-	border: 1px solid #AAA;
+	border: 1px solid #aaa;
 	border-radius: 5px;
 }
 
 .painel .cabecalho {
 	width: 100%;
-	background-color: #DDD;
+	background-color: #ddd;
 	padding: 10px 0px;
 	border-radius: 5px;
 	font-size: 1.4rem;
@@ -135,8 +165,8 @@ body {
 	padding: 10px 20px;
 	font-size: 1.4rem;
 	border-radius: 5px;
-	color: #FFF;
-	background-color: #2196F3;
+	color: #fff;
+	background-color: #2196f3;
 }
 
 #app h1 {
