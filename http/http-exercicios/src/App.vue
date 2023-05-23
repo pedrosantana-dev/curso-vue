2b9a27ff05ce2deb33486890157321514fb7fe09
Pedro Santana <pedromanu27@gmail.com>
2023-05-23 19:59:40 -0300

    http crud

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-alert show dismissible v-for="mensagem in mensagens" :key="mensagem.text" :variant="mensagem.tipo">{{
+			mensagem.texto }}</b-alert>
 		<b-card>
 			<b-form-group label="Nome:">
 				<b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o Nome"></b-form-input>
@@ -17,7 +19,9 @@
 			<b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
 				<strong>Nome:</strong> {{ usuario.nome }}<br>
 				<strong>Email:</strong> {{ usuario.email }}<br>
-				<strong>ID:</strong>{{ id }}
+				<strong>ID:</strong>{{ id }}<br>
+				<b-button variant="warning" size="lg" @click="carregar(id)">Carregar</b-button>
+				<b-button variant="danger" size="lg" class="ml-2" @click="excluir(id)">Excluir</b-button>
 			</b-list-group-item>
 		</b-list-group>
 	</div>
@@ -27,18 +31,44 @@
 export default {
 	data() {
 		return {
+			id: null,
 			usuario: {
 				nome: '',
 				email: ''
 			},
-			usuarios: []
+			usuarios: [],
+			mensagens: []
 		}
 	},
 	methods: {
+		limpar() {
+			this.usuario.nome = '',
+				this.usuario.email = '',
+				this.id = null
+		},
+		carregar(id) {
+			this.id = id
+			this.usuario = { ...this.usuarios[id] }
+		},
+		excluir(id) {
+			this.$http.delete(`usuarios/${id}.json`)
+				.then(() => this.limpar())
+				.catch(_ => {
+					this.mensagens.push({
+						texto: 'Problema para excluir!',
+						tipo: 'danger'
+					})
+				})
+		},
 		salvar() {
-			this.$http.post('usuarios.json', this.usuario).then(res => {
-				this.usuario.nome = '',
-				this.usuario.email = ''
+			const metodo = this.id ? 'patch' : 'post'
+			const finalUrl = this.id ? `/${this.id}.json` : '.json'
+			this.$http[metodo](`usuarios${finalUrl}`, this.usuario).then(_ => {
+				this.limpar()
+				this.mensagens.push({
+					texto: 'Operação realizada com sucesso!',
+					tipo: 'success'
+				})
 			})
 		},
 		obterUsuarios() {
