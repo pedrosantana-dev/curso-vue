4cf292ca61aa96dd0575ea5079940e2bc3870d07
Pedro Santana <pedromanu27@gmail.com>
2023-06-24 18:38:04 -0300

    feat: :sparkles: validacoes de funds

--- projeto-03-acoes/stock-trader/src/components/portfolio/Stock.vue
+++ projeto-03-acoes/stock-trader/src/components/portfolio/Stock.vue
@@ -10,9 +10,11 @@
         </v-card>
         <v-card>
             <v-container fill-height>
-                <v-text-field label="Quantidade" type="number" v-model.number="quantity" />
-                <v-btn class="blue darken-3 white--text" :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                    @click="sellStock">Vender</v-btn>
+                <v-text-field label="Quantidade" type="number" 
+                :error="insufficientQuantity || !Number.isInteger(quantity)" v-model.number="quantity" />
+                <v-btn class="blue darken-3 white--text" 
+                    :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)"
+                    @click="sellStock">{{insufficientQuantity ? 'Insuficiente' : 'Vender'}}</v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -25,6 +27,11 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed: {
+        insufficientQuantity() {
+            return this.quantity > this.stock.quantity
         }
     },
     methods: {
