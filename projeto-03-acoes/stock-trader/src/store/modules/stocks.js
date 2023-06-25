bddbfab609007e9f6bfd89a67e94ca78f1af5cb7
Pedro Santana <pedromanu27@gmail.com>
2023-06-25 11:17:18 -0300

    feat: :sparkles: finalizar o dia

--- projeto-03-acoes/stock-trader/src/store/modules/stocks.js
+++ projeto-03-acoes/stock-trader/src/store/modules/stocks.js
@@ -7,16 +7,24 @@ export default {
     mutations: {
         setStocks(state, stocks) {
             state.stocks = stocks
+        },
+        randomizeStocks(state) {
+            state.stocks.forEach(stock => {
+                stock.price = Math.round(stock.price * (1 + Math.random() - 0.45))
+            })
         }
     },
     actions: {
         buyStock({ commit }, order) {
             commit('buyStock', order)
         },
-        initStocks({ commit } ) {
+        initStocks({ commit }) {
             // eslint-disable-next-line
             console.log('initStocks')
             commit('setStocks', stocks)
+        },
+        randomizeStocks({ commit }) {
+            commit('randomizeStocks')
         }
     },
     getters: {
